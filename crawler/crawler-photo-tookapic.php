4e52c084875203845091d32258cd9aee316670d5
thuannvn <ninhthuan82us@gmail.com>
2016-06-26 17:10:30 +0700

    Add library simplehtmldom for crawler

---
+++ crawler/crawler-photo-tookapic.php
@@ -0,0 +1,17 @@
+<?php 
+	define('DRUPAL_ROOT', '/home/vhosts/stock-demo.techblogsearch.com');
+	require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+	drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+	include 'functions.php';
+
+	$proxy = "";
+	
+	// Processing for 1 detail url https://tookapic.com/photos/122676
+	$photo_url = "https://tookapic.com/photos/122676";
+
+?>
+
+<?php 
+// Your functions here
+
+?>
---
+++ crawler/functions.php
@@ -0,0 +1,10 @@
+<?php 
+define('DRUPAL_ROOT', '/home/vhosts/stock-demo.techblogsearch.com');
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
+require_once('simple_html_dom.php');
+
+// Define your common functions here
+
+?>
