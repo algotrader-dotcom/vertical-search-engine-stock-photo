08f34e792f9d907698bc61c8138b05829ab79bfa
thuannvn <ninhthuan82us@gmail.com>
2016-06-26 17:20:33 +0700

    Update crawler code

--- crawler/crawler-photo-tookapic.php
+++ crawler/crawler-photo-tookapic.php
@@ -8,10 +8,22 @@
 	
 	// Processing for 1 detail url https://tookapic.com/photos/122676
 	$photo_url = "https://tookapic.com/photos/122676";
+	$node_photo = photo_url_parser($photo_url); // Photo html parser
 
 ?>
 
 <?php 
-// Your functions here
-
+// Get reponse html data then parser title, description, tags, photo url into array
+function photo_url_parser($url, $proxy){
+	$nodes = array();
+	$html = getResponse($url,$proxy);
+		
+	if ($html){
+		return $nodes;
+		
+	} else {
+		print "Đã có lỗi xay rả khi get response from ".$url."\n"; return;
+	}
+	return $nodes;
+}
 ?>
--- crawler/functions.php
+++ crawler/functions.php
@@ -5,6 +5,32 @@ drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
 require_once('simple_html_dom.php');
 
-// Define your common functions here
 
+// Get html response data from URL 
+function getResponse($url, $proxy){
+	$html = "";
+	$ch = curl_init();	
+	$agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
+    $timeout = 0; // set to zero for no timeout	
+	if ($proxy != ""){
+		curl_setopt($ch, CURLOPT_PROXY, $proxy);
+	}
+	
+	curl_setopt($ch, CURLOPT_VERBOSE, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+	curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_ENCODING , "gzip"); 
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	$http_data = curl_exec($ch);
+	
+	$http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	print "getResponse: status code: ".$http_status."\n";
+
+	curl_close($ch);
+	
+	$html = str_get_html($http_data);
+	return $html;
+}
 ?>
